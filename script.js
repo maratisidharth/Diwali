58d4078cf60cf0828e165a673103b996a7d21569
Marati Siddhartha <maratisiddhartha@gmail.com>
2026-09-18 09:52:25 +0530

    Diwali

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const whatsappButtons = document.querySelectorAll(".whatsapp-btn");
+
+
+    const whatsappNumber = "6302898225";
+
+    whatsappButtons.forEach(function (button) {
+
+        button.addEventListener("click", function (event) {
+
+            event.preventDefault();
+
+            const product = button.getAttribute("data-product");
+
+            const message =
+                "Hello, I would like to get more details about  HAPPY DIWALI " +
+                product + 
+                ".";
+
+            const whatsappURL =
+                "https://wa.me/" +
+                whatsappNumber +
+                "?text=" +
+                encodeURIComponent(message);
+
+            window.open(whatsappURL, "_blank");
+        });
+
+    });
+
+});
